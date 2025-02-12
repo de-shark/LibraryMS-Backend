5d4898dbfadf587a4efd326723640086ff710791
DE_SHARK <2799045359@qq.com>
2025-02-12 12:37:29 +0800

    添加 SecurityConfig

--- interfaces/build.gradle.kts
+++ interfaces/build.gradle.kts
@@ -4,4 +4,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
 } 
---
+++ interfaces/src/main/java/me/deshark/lms/interfaces/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package me.deshark.lms.interfaces.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author DE_SHARK
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/test").permitAll()
+                    .anyRequest().authenticated()
+            )
+            // 禁用 CSRF 保护（适合 API 服务）
+            .csrf(AbstractHttpConfigurer::disable)
+            // 禁用 HTTP Basic 认证
+            .httpBasic(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
