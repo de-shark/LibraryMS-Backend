7fdb7d0bff318a76e55185ada219345fac62710b
DE_SHARK <2799045359@qq.com>
2025-02-12 15:03:22 +0800

    更新 user 表

--- Docker/PostgreSQL/CreateTables.sql
+++ Docker/PostgreSQL/CreateTables.sql
@@ -1,13 +1,13 @@
 -- 创建 ENUM 类型
-CREATE TYPE user_role AS ENUM ('reader', 'librarian', 'admin');
-CREATE TYPE user_status AS ENUM ('unverified', 'activated', 'banned');
+CREATE TYPE user_role AS ENUM ('READER', 'LIBRARIAN', 'ADMIN');
+CREATE TYPE user_status AS ENUM ('UNVERIFIED', 'ACTIVATED', 'BANNED');
 
 CREATE TABLE "users" (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    role user_role NOT NULL DEFAULT 'reader',
-    status user_status NOT NULL DEFAULT 'unverified'
+    role user_role NOT NULL DEFAULT 'READER',
+    status user_status NOT NULL DEFAULT 'UNVERIFIED'
 )
