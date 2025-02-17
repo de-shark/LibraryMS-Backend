2947f7c9f41670fb6bd80e4660ce389c553c39fc
DE_SHARK <2799045359@qq.com>
2025-02-17 22:12:51 +0800

    fix: 增加 uuid-creator 依赖

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,6 +3,9 @@ dependencies {
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
+    // https://mvnrepository.com/artifact/com.github.f4b6a3/uuid-creator
+    implementation("com.github.f4b6a3:uuid-creator:6.0.0")
+
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
     // https://mvnrepository.com/artifact/org.mockito/mockito-core
