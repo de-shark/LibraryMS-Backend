a8b9d3822cd5c96c22faed0ee766f549391982c5
DE_SHARK <2799045359@qq.com>
2025-02-25 22:26:50 +0800

    fix(application/build.gradle.kts): 修复app对infr的依赖

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":domain"))
+    implementation(project(":infrastructure"))
     implementation(project(":common"))
 
     implementation("org.springframework.boot:spring-boot-starter")
