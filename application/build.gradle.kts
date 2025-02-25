bf61708a5a3ebfd250fa20aa2329d6a7b3636ee7
DE_SHARK <2799045359@qq.com>
2025-02-25 23:56:16 +0800

    chore(build.gradle.kts): 优化模块间的依赖

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":infrastructure"))
     implementation(project(":common"))
 
     implementation("org.springframework.boot:spring-boot-starter")
--- start/build.gradle.kts
+++ start/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":interfaces"))
+    implementation(project(":infrastructure"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
