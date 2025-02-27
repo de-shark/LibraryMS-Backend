3dca3e7a23673d2b16f758bda9c4b2b8a18a132b
DE_SHARK <2799045359@qq.com>
2025-02-27 14:41:10 +0800

    feat: add CRUD operations to BookInfoMapper

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/BookInfoMapper.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/BookInfoMapper.java
@@ -1,11 +1,71 @@
 package me.deshark.lms.infrastructure.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import me.deshark.lms.infrastructure.entity.BookInfoDO;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
- * @author DE_SHARK
- * @date 2025/2/26 19:27
+ * 图书信息数据访问接口
+ * @author deshark
  */
 @Mapper
 public interface BookInfoMapper {
+
+    /**
+     * 根据ISBN查询图书信息
+     */
+    @Select("SELECT * FROM book_info WHERE isbn = #{isbn}")
+    Optional<BookInfoDO> findByIsbn(@Param("isbn") String isbn);
+
+    /**
+     * 插入图书信息
+     */
+    @Insert("INSERT INTO book_info (isbn, title, author, publisher, publish_year, description, cover_url) " +
+            "VALUES (#{isbn}, #{title}, #{author}, #{publisher}, #{publishYear}, #{description}, #{coverUrl})")
+    int insert(BookInfoDO bookInfoDO);
+
+    /**
+     * 更新图书信息
+     */
+    @Update("UPDATE book_info SET " +
+            "title = #{title}, " +
+            "author = #{author}, " +
+            "publisher = #{publisher}, " +
+            "publish_year = #{publishYear}, " +
+            "description = #{description}, " +
+            "cover_url = #{coverUrl} " +
+            "WHERE isbn = #{isbn}")
+    int update(BookInfoDO bookInfoDO);
+
+    /**
+     * 删除图书信息
+     */
+    @Delete("DELETE FROM book_info WHERE isbn = #{isbn}")
+    int deleteByIsbn(@Param("isbn") String isbn);
+
+    /**
+     * 查询所有图书信息
+     */
+    @Select("SELECT * FROM book_info ORDER BY created_at DESC")
+    List<BookInfoDO> findAll();
+
+    /**
+     * 根据标题模糊查询图书信息
+     */
+    @Select("SELECT * FROM book_info WHERE title LIKE CONCAT('%', #{title}, '%')")
+    List<BookInfoDO> findByTitleContaining(@Param("title") String title);
+
+    /**
+     * 根据作者查询图书信息
+     */
+    @Select("SELECT * FROM book_info WHERE author = #{author}")
+    List<BookInfoDO> findByAuthor(@Param("author") String author);
+
+    /**
+     * 统计图书数量
+     */
+    @Select("SELECT COUNT(*) FROM book_info")
+    long count();
 }
