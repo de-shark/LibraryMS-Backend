b2adfccb0c35e9a5f53cb4c8faa0c49a83ef7c8c
DE_SHARK <2799045359@qq.com>
2025-02-27 16:41:41 +0800

    feat: add BookController with basic structure and routing

---
+++ interfaces/src/main/java/me/deshark/lms/interfaces/controller/BookController.java
@@ -0,0 +1,13 @@
+package me.deshark.lms.interfaces.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author DE_SHARK
+ */
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+}
