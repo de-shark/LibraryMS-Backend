d3d44355b4cff7c2118f18eda39bb90ba4003043
DE_SHARK <2799045359@qq.com>
2025-02-27 14:30:23 +0800

    feat: 添加BookCopyMapper和BookInfoMapper接口

---
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/BookCopyMapper.java
@@ -0,0 +1,11 @@
+package me.deshark.lms.infrastructure.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author DE_SHARK
+ * @date 2025/2/26 19:28
+ */
+@Mapper
+public class BookCopyMapper {
+}
