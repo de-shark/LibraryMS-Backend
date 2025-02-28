8a2336f3fbb9ed40eeaa5ab8bbd24b6b871b45c3
DE_SHARK <2799045359@qq.com>
2025-02-28 17:17:26 +0800

    feat: Add creditScore and updatedAt fields to UserDO

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/entity/UserDO.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/entity/UserDO.java
@@ -18,7 +18,9 @@ public class UserDO {
     private String username;
     private String password;
     private String email;
-    private Timestamp createdAt;
     private UserRole role;
     private UserStatus status;
+    private int creditScore;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 }
