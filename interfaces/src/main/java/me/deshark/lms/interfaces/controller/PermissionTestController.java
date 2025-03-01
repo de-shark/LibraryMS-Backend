f87deb6f0999fa7f8225a547046173389254cf19
DE_SHARK <2799045359@qq.com>
2025-03-01 11:02:40 +0800

    feat: add PermissionTestController for API testing

---
+++ interfaces/src/main/java/me/deshark/lms/interfaces/controller/PermissionTestController.java
@@ -0,0 +1,20 @@
+package me.deshark.lms.interfaces.controller;
+
+import me.deshark.lms.interfaces.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author DE_SHARK
+ */
+@RestController
+@RequestMapping("/api/test")
+public class PermissionTestController {
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Void>> test() {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+}
