7092f8848cca91493404a44c67c57a73936c721a
DE_SHARK <2799045359@qq.com>
2025-03-02 14:53:37 +0800

    refactor(GlobalExceptionHandler): handling Exception

--- interfaces/src/main/java/me/deshark/lms/interfaces/exception/GlobalExceptionHandler.java
+++ interfaces/src/main/java/me/deshark/lms/interfaces/exception/GlobalExceptionHandler.java
@@ -2,13 +2,11 @@ package me.deshark.lms.interfaces.exception;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.deshark.lms.interfaces.dto.ResultBody;
+import me.deshark.lms.interfaces.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
 
 /**
  * @author DE_SHARK
@@ -20,21 +18,16 @@ public class GlobalExceptionHandler {
 
     // 400 Bad Request
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(ApiResponse.<Void>builder().error(e.getMessage()).build());
     }
 
     // 处理其他未捕获异常
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResultBody<Void>> handleUnexpectedException(Exception e) {
+    public ResponseEntity<ApiResponse<Void>> handleUnexpectedException(Exception e) {
         log.error("Unexpected error", e);
-        ResultBody<Void> resultBody = ResultBody.<Void>builder()
-                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-                .data(null)
-                .build();
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(resultBody);
+                .body(ApiResponse.<Void>builder().error(e.getMessage()).build());
     }
 }
