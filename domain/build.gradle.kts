cf22a866123b553d49130a359f29e2c422a179fd
DE_SHARK <2799045359@qq.com>
2025-03-02 15:37:09 +0800

    refactor(domain): remove test dependencies from build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,6 @@
 dependencies {
     implementation(project(":common"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-    testImplementation("org.mockito:mockito-core:5.15.2")
-
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
