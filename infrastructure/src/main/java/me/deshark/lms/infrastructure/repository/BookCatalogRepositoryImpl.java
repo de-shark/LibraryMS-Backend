f76da9eba20eb929f78ac3f2b5ce6388c7fa4a4f
DE_SHARK <2799045359@qq.com>
2025-03-03 16:20:57 +0800

    feat: implement update method in BookCatalogRepositoryImpl

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/BookCatalogRepositoryImpl.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/BookCatalogRepositoryImpl.java
@@ -55,6 +55,14 @@ public class BookCatalogRepositoryImpl implements BookCatalogRepository {
 
     @Override
     public void update(BookCatalog bookCatalog) {
-
+        BookCatalogDO bookCatalogDO = BookCatalogDO.builder()
+                .isbn(bookCatalog.getIsbn().toString())
+                .title(bookCatalog.getTitle())
+                .author(bookCatalog.getAuthor())
+                .publisher(bookCatalog.getPublisher())
+                .publishYear(bookCatalog.getPublishedYear())
+                .description(bookCatalog.getDescription())
+                .build();
+        bookCatalogMapper.update(bookCatalogDO);
     }
 }
