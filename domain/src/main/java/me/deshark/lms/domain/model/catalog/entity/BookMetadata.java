f83f3338a316a79da67eecbc411bec76e4183ce6
DE_SHARK <2799045359@qq.com>
2025-03-04 09:26:01 +0800

    refactor(domain): refactor BookMetadata entity

--- domain/src/main/java/me/deshark/lms/domain/model/catalog/entity/BookMetadata.java
+++ domain/src/main/java/me/deshark/lms/domain/model/catalog/entity/BookMetadata.java
@@ -1,29 +1,21 @@
 package me.deshark.lms.domain.model.catalog.entity;
 
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import me.deshark.lms.domain.model.catalog.vo.Isbn;
+
+import java.util.Date;
 
 /**
  * @author DE_SHARK
  * @date 2025/2/15 20:47
  */
-@Getter
+@Data
 @Builder
 public class BookMetadata {
     private final Isbn isbn;
     private String title;
     private String author;
     private String publisher;
-    private int publishedYear;
-    private String description;
-    private String coverUrl;
-
-    public void updateDetails(String title, String author, String publisher, int publishedYear, String description) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.publishedYear = publishedYear;
-        this.description = description;
-    }
+    private Date publishedDate;
 }
