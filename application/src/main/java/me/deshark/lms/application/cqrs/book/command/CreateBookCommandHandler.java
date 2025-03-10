185a4191e85b8ac4797d2b17243c56733221fdf0
DE_SHARK <2799045359@qq.com>
2025-03-10 22:49:56 +0800

    feat: update CreateBookCommandHandler to fetch and save BookMetadata from provider

--- application/src/main/java/me/deshark/lms/application/cqrs/book/command/CreateBookCommandHandler.java
+++ application/src/main/java/me/deshark/lms/application/cqrs/book/command/CreateBookCommandHandler.java
@@ -1,6 +1,7 @@
 package me.deshark.lms.application.cqrs.book.command;
 
 import lombok.RequiredArgsConstructor;
+import me.deshark.lms.domain.model.catalog.entity.BookMetadata;
 import me.deshark.lms.domain.model.catalog.exception.BookAlreadyExistsException;
 import me.deshark.lms.domain.model.catalog.vo.Isbn;
 import me.deshark.lms.domain.repository.BookRepository;
@@ -42,8 +43,9 @@ public class CreateBookCommandHandler {
         }
 
         // 调用 API 获取图书信息
+        BookMetadata bookMetadata = bookMetadataProvider.fetch(isbn);
 
         // 保存到仓库
-        bookRepository.save(null);
+        bookRepository.save(bookMetadata);
     }
 }
