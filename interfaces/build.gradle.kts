5160b267cd592a90d3b465d9e39d774e716bc9bd
DE_SHARK <2799045359@qq.com>
2025-03-10 15:26:32 +0800

    chore: downgrade Spring Boot version to 3.2.12 in build.gradle.kts

--- interfaces/build.gradle.kts
+++ interfaces/build.gradle.kts
@@ -1,10 +1,8 @@
 dependencies {
     implementation(project(":application"))
     implementation(project(":common"))
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     compileOnly("org.projectlombok:lombok:1.18.36")
