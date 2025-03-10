59450eea10bbbc7009b62afbd01a7ca7286827ed
DE_SHARK <2799045359@qq.com>
2025-03-10 21:21:53 +0800

    feat: add external secrets configuration for Google Books API key

--- start/src/main/java/me/deshark/lms/LmsApplication.java
+++ start/src/main/java/me/deshark/lms/LmsApplication.java
@@ -2,8 +2,13 @@ package me.deshark.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+/**
+ * @author deshark
+ */
 @SpringBootApplication(scanBasePackages = "me.deshark.lms")
+@PropertySource(value = "classpath:secrets.properties", ignoreResourceNotFound = true)
 public class LmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(LmsApplication.class, args);
