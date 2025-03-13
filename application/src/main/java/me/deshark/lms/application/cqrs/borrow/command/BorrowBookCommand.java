ac828acb957fb749b08393947c0166b22b9d5026
DE_SHARK <2799045359@qq.com>
2025-03-13 10:19:11 +0800

    feat: add BorrowBookCommand and Handler

---
+++ application/src/main/java/me/deshark/lms/application/cqrs/borrow/command/BorrowBookCommand.java
@@ -0,0 +1,27 @@
+package me.deshark.lms.application.cqrs.borrow.command;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+/**
+ * 借阅图书命令
+ *
+ * <p>包含读者借阅图书所需的必要信息</p>
+ *
+ * @author DE_SHARK
+ */
+@Data
+@Builder
+public class BorrowBookCommand {
+    /**
+     * 读者ID
+     */
+    private final UUID patronId;
+
+    /**
+     * 图书ISBN
+     */
+    private final String isbn;
+}
