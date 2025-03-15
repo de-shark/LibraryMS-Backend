174432c93ead6107754046e35d0e4011817766e9
DE_SHARK <2799045359@qq.com>
2025-03-15 21:44:57 +0800

    refactor(Patron): remove creditScore and MIN_CREDIT_SCORE fields to simplify borrowing logic

--- domain/src/main/java/me/deshark/lms/domain/model/borrowing/entity/Patron.java
+++ domain/src/main/java/me/deshark/lms/domain/model/borrowing/entity/Patron.java
@@ -16,11 +16,8 @@ public class Patron {
     private final UUID id;
     // 目前已借阅数量
     private int currentBorrows;
-    // 信用分
-    private int creditScore;
     
     private static final int MAX_BORROWS = 5;
-    private static final int MIN_CREDIT_SCORE = 60;
 
     public void canBorrow() {
         if (currentBorrows >= MAX_BORROWS) {
