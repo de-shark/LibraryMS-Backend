2873bd49baa40d1b23a5ace1f1e776704a7312fc
DE_SHARK <2799045359@qq.com>
2025-03-18 18:18:29 +0800

    feat: Add user role and borrower info insertion in UserRepositoryImpl

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/UserRoleMapper.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/UserRoleMapper.java
@@ -1,6 +1,7 @@
 package me.deshark.lms.infrastructure.mapper;
 
 import me.deshark.lms.infrastructure.entity.UserRoleDO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -13,6 +14,9 @@ import java.util.UUID;
  */
 @Mapper
 public interface UserRoleMapper {
+
+    @Insert("INSERT INTO user_role (user_role_id, user_id, role) VALUES (#{userRoleId}, #{userId}, #{role})")
+    void insert(UserRoleDO userRoleDO);
 
     @Select("SELECT * FROM user_role WHERE user_id = #{uuid}")
     Optional<UserRoleDO> findByUserId(@Param("uuid") UUID userId);
--- infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/UserRepositoryImpl.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/UserRepositoryImpl.java
@@ -1,16 +1,19 @@
 package me.deshark.lms.infrastructure.repository;
 
 import lombok.RequiredArgsConstructor;
+import me.deshark.lms.common.utils.GUID;
 import me.deshark.lms.domain.model.auth.entity.AuthUser;
+import me.deshark.lms.domain.model.auth.vo.UserRole;
 import me.deshark.lms.domain.repository.auth.UserRepository;
+import me.deshark.lms.infrastructure.entity.BorrowerInfoDO;
 import me.deshark.lms.infrastructure.entity.UserDO;
 import me.deshark.lms.infrastructure.entity.UserRoleDO;
+import me.deshark.lms.infrastructure.enums.UserRoleType;
+import me.deshark.lms.infrastructure.mapper.BorrowerInfoMapper;
 import me.deshark.lms.infrastructure.mapper.UserMapper;
 import me.deshark.lms.infrastructure.mapper.UserPersistenceMapper;
 import me.deshark.lms.infrastructure.mapper.UserRoleMapper;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 /**
  * @author DE_SHARK
@@ -22,6 +25,7 @@ public class UserRepositoryImpl implements UserRepository {
     private final UserMapper userMapper;
     private final UserRoleMapper userRoleMapper;
     private final UserPersistenceMapper persistenceMapper;
+    private final BorrowerInfoMapper borrowerInfoMapper;
 
     @Override
     public boolean existsByUsername(String username) {
@@ -34,19 +38,33 @@ public class UserRepositoryImpl implements UserRepository {
         if (userMapper.insert(userDO) != 1) {
             throw new IllegalArgumentException("User not saved");
         }
-        ;
+        // 插入用户角色
+        UserRoleDO userRoleDO = UserRoleDO.builder()
+                .userRoleId(GUID.v7())
+                .userId(userDO.getUuid())
+                .role(UserRoleType.USER)
+                .build();
+        userRoleMapper.insert(userRoleDO);
+        // 插入借阅者信息
+        BorrowerInfoDO borrowerInfoDO = BorrowerInfoDO.builder()
+                .id(userDO.getUuid())
+                .maxBorrowLimit(5)
+                .currentLoans(0)
+                .build();
+        borrowerInfoMapper.insert(borrowerInfoDO);
     }
 
     @Override
     public AuthUser findByUsername(String username) {
-        Optional<UserDO> userDO = userMapper.findByUsername(username);
-        if (userDO.isEmpty()) {
-            throw new IllegalArgumentException("User not found");
-        }
-        Optional<UserRoleDO> userRoleDO = userRoleMapper.findByUserId(userDO.get().getUuid());
+        UserDO userDO = userMapper.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Username not found"));
 
-        AuthUser authUser = persistenceMapper.toDomainEntity(userDO.get());
-        authUser.setUserRoleType(userRoleDO.map(UserRoleDO::getRole).orElse(null));
+        UserRoleDO userRoleDO = userRoleMapper.findByUserId(userDO.getUuid())
+                .orElseThrow(() -> new IllegalArgumentException("Null Role Type"));
+
+        UserRole userRole = UserRole.valueOf(userRoleDO.getRole().toString());
+        AuthUser authUser = persistenceMapper.toDomainEntity(userDO);
+        authUser.setUserRole(userRole);
         return authUser;
     }
 }
