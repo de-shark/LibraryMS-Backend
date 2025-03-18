f6782aa73269a6580b6f7556e85a022155a8e6b7
DE_SHARK <2799045359@qq.com>
2025-03-18 17:17:46 +0800

    feat: add Lombok annotations to BorrowerInfoDO

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/entity/BorrowerInfoDO.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/entity/BorrowerInfoDO.java
@@ -1,10 +1,15 @@
 package me.deshark.lms.infrastructure.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.UUID;
 
 /**
  * @author DE_SHARK
  */
+@Data
+@Builder
 public class BorrowerInfoDO {
     private UUID id;
     private int maxBorrowLimit;
