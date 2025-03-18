d284de0240b9b22ae0b2cabc809862197ea443cf
DE_SHARK <2799045359@qq.com>
2025-03-18 19:07:59 +0800

    feat: Add Patron conversion and findByUsername implementation in PatronRepositoryImpl

--- infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/BorrowerInfoMapper.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/mapper/BorrowerInfoMapper.java
@@ -1,5 +1,6 @@
 package me.deshark.lms.infrastructure.mapper;
 
+import me.deshark.lms.domain.model.borrowing.entity.Patron;
 import me.deshark.lms.infrastructure.entity.BorrowerInfoDO;
 import org.apache.ibatis.annotations.*;
 
@@ -18,28 +19,32 @@ public interface BorrowerInfoMapper {
 
     @Update("UPDATE borrower_info SET " +
             "max_borrow_limit = #{maxBorrowLimit}, " +
-            "current_loans = #{currentLoans}, " +
+            "current_loans = #{currentLoans}  " +
             "WHERE user_id = #{userId}")
     void update(BorrowerInfoDO borrowerInfoDO);
+
+    @Delete("DELETE FROM borrower_info WHERE user_id = #{userId}")
+    void delete(@Param("userId") UUID userId);
 
     @Select("SELECT * FROM borrower_info WHERE user_id = #{userId}")
     @Results(id = "borrowerInfoMap", value = {
             @Result(property = "userId", column = "user_id"),
             @Result(property = "maxBorrowLimit", column = "max_borrow_limit"),
-            @Result(property = "currentLoans", column = "current_loans"),
-            @Result(property = "totalFines", column = "total_fines")
+            @Result(property = "currentLoans", column = "current_loans")
     })
     Optional<BorrowerInfoDO> findByUserId(@Param("userId") UUID userId);
 
     @Update("UPDATE borrower_info SET current_loans = current_loans + #{delta} WHERE user_id = #{userId}")
     void updateCurrentLoans(@Param("userId") UUID userId, @Param("delta") int delta);
 
-    @Delete("DELETE FROM borrower_info WHERE user_id = #{userId}")
-    void delete(@Param("userId") UUID userId);
-
     @Select("SELECT max_borrow_limit FROM borrower_info WHERE user_id = #{userId}")
     int getBorrowLimit(@Param("userId") UUID userId);
 
-    @Select("SELECT total_fines FROM borrower_info WHERE user_id = #{userId}")
-    double getTotalFines(@Param("userId") UUID userId);
+    static Patron convertToPatron(BorrowerInfoDO borrowerInfoDO) {
+        return Patron.builder()
+                .id(borrowerInfoDO.getUserId())
+                .maxBorrowLimit(borrowerInfoDO.getMaxBorrowLimit())
+                .currentBorrows(borrowerInfoDO.getCurrentLoans())
+                .build();
+    }
 }
--- infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/PatronRepositoryImpl.java
+++ infrastructure/src/main/java/me/deshark/lms/infrastructure/repository/PatronRepositoryImpl.java
@@ -1,16 +1,28 @@
 package me.deshark.lms.infrastructure.repository;
 
+import lombok.RequiredArgsConstructor;
 import me.deshark.lms.domain.model.borrowing.entity.Patron;
 import me.deshark.lms.domain.repository.borrow.PatronRepository;
+import me.deshark.lms.infrastructure.entity.BorrowerInfoDO;
+import me.deshark.lms.infrastructure.entity.UserDO;
+import me.deshark.lms.infrastructure.entity.UserRoleDO;
+import me.deshark.lms.infrastructure.mapper.BorrowerInfoMapper;
+import me.deshark.lms.infrastructure.mapper.UserMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
  * @author DE_SHARK
  */
 @Repository
+@RequiredArgsConstructor
 public class PatronRepositoryImpl implements PatronRepository {
+
+    private final UserMapper userMapper;
+    private final BorrowerInfoMapper borrowerInfoMapper;
+
     @Override
     public Patron findById(UUID id) {
         return null;
@@ -18,6 +30,12 @@ public class PatronRepositoryImpl implements PatronRepository {
 
     @Override
     public Patron findByUsername(String username) {
-        return null;
+        // 1. 获取用户id
+        UserDO userDO = userMapper.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("用户不存在"));
+        // 2. 通过用户id查借阅信息
+        return borrowerInfoMapper.findByUserId(userDO.getUuid())
+                .map(BorrowerInfoMapper::convertToPatron)
+                .orElseThrow(() -> new IllegalArgumentException("找不到个人借阅信息"));
     }
 }
