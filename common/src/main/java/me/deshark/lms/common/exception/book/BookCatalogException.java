914a07554c8348f8e893f4609b16e409e89984c4
DE_SHARK <2799045359@qq.com>
2025-03-19 23:18:23 +0800

    feat: Add custom exceptions for book catalog and borrow operations

---
+++ common/src/main/java/me/deshark/lms/common/exception/book/BookCatalogException.java
@@ -0,0 +1,10 @@
+package me.deshark.lms.common.exception.book;
+
+/**
+ * @author DE_SHARK
+ */
+public class BookCatalogException extends RuntimeException {
+    public BookCatalogException(String message) {
+        super(message);
+    }
+}
---
+++ common/src/main/java/me/deshark/lms/common/exception/borrow/BorrowBookException.java
@@ -0,0 +1,10 @@
+package me.deshark.lms.common.exception.borrow;
+
+/**
+ * @author DE_SHARK
+ */
+public class BorrowBookException extends RuntimeException {
+    public BorrowBookException(String message) {
+        super(message);
+    }
+}
