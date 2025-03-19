5dea941d8b497aebfd36d098a84169bfb91da6e3
DE_SHARK <2799045359@qq.com>
2025-03-19 18:45:00 +0800

    refactor: introduce BookCopyStatus enum and add static factory method `copyOf` for creation

--- domain/src/main/java/me/deshark/lms/domain/model/catalog/entity/BookCopy.java
+++ domain/src/main/java/me/deshark/lms/domain/model/catalog/entity/BookCopy.java
@@ -2,6 +2,8 @@ package me.deshark.lms.domain.model.catalog.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import me.deshark.lms.common.utils.GUID;
+import me.deshark.lms.domain.model.catalog.BookCopyStatus;
 import me.deshark.lms.domain.model.catalog.vo.Isbn;
 
 import java.util.UUID;
@@ -16,6 +18,15 @@ import java.util.UUID;
 public class BookCopy {
     private final UUID copyId;
     private final Isbn isbn;
-    private String status;
+    private BookCopyStatus status;
     private int loanCount;
+
+    public static BookCopy copyOf(Isbn isbn) {
+        return BookCopy.builder()
+                .copyId(GUID.v7())
+                .isbn(isbn)
+                .status(BookCopyStatus.AVAILABLE)
+                .loanCount(0)
+                .build();
+    }
 }
