4cb832a164e9f599e9f55a604b3c06d82a33ebb1
DE_SHARK <2799045359@qq.com>
2025-04-02 22:36:16 +0800

    feat: add MinIO configuration and dependencies

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    implementation("io.minio:minio:8.5.17")
 }
